51592d38e70c0d5cbb18d18283cac55320d350b8
Tsungyu Yu <ytyubox@gmail.com>
2020-04-28 14:04:48 +0800

    decoder create

--- Sources/PlainTextCoder/PlainTextCoder.swift
+++ Sources/PlainTextCoder/PlainTextCoder.swift
@@ -1,3 +1,19 @@
-struct PlainTextCoder {
-    var text = "Hello, World!"
+final class PlainTextDecoder:Decoder {
+  var codingPath: [CodingKey] = [ ]
+  
+  var userInfo: [CodingUserInfoKey : Any] =  [:]
+  
+  func container<Key>(keyedBy type: Key.Type) throws -> KeyedDecodingContainer<Key> where Key : CodingKey {
+    fatalError()
+  }
+  
+  func unkeyedContainer() throws -> UnkeyedDecodingContainer {
+    fatalError()
+  }
+  
+  func singleValueContainer() throws -> SingleValueDecodingContainer {
+    fatalError()
+  }
+  
+    
 }
--- Tests/PlainTextCoderTests/PlainTextCoderTests.swift
+++ Tests/PlainTextCoderTests/PlainTextCoderTests.swift
@@ -6,7 +6,7 @@ final class PlainTextCoderTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PlainTextCoder().text, "Hello, World!")
+        
     }
 
     static var allTests = [
